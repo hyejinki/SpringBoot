49f4545b749ac572fe28440a1c795cdfc7b9b05f
hyejinki <zlagg23@gmail.com>
2024-01-04 17:50:07 +0900

    프로젝트 생성

--- jpashop/src/test/java/jpabook/jpashop/JpashopApplicationTests.java
+++ jpashop/src/test/java/jpabook/jpashop/JpashopApplicationTests.java
@@ -1,10 +1,10 @@
-package vito.test;
+package jpabook.jpashop;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class TestApplicationTests {
+class JpashopApplicationTests {
 
 	@Test
 	void contextLoads() {
