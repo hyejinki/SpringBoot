b70bd1dae328c8425be27f28f173e46106a29a4b
hyejinki <zlagg23@gmail.com>
2024-01-18 21:18:03 +0900

    jpa basic

---
+++ jpa-basic/src/main/java/jpabasic/jpabasic/JpaBasicApplication.java
@@ -0,0 +1,18 @@
+package jpabasic.jpabasic;
+
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.PersistenceContext;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JpaBasicApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JpaBasicApplication.class, args);
+
+
+
+	}
+
+}
